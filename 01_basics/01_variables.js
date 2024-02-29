de09ca8427585a97fe349f30152623cd679f6987
harsh shrama <161470818+harshsharma06@users.noreply.github.com>
2024-02-29 19:27:46 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountID="9027090491"
+let accEmail="harsh@mait.com"
+var password="11223344"
+accountCity="mathura"
+
+//accountID="9520809058"-->not applicable
+
+password="55443322"
+accountCity="dehradun"
+
+console.table([accountID,accEmail,password,accountCity])
