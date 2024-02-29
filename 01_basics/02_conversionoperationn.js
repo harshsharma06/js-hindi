77542c58f76eb558c399227e5a12af8b7c8ffa83
harsh shrama <161470818+harshsharma06@users.noreply.github.com>
2024-02-29 20:06:32 +0000

    conversion of variables

---
+++ 01_basics/02_conversionoperationn.js
@@ -0,0 +1,20 @@
+let score="69abc"
+
+console.log(typeof(score));
+
+let ValueinNumber= Number(score)
+
+console.log(typeof(ValueinNumber));
+
+console.log(ValueinNumber)//Nan=>not a number
+
+//1=>true, 0=>false
+//""=>false
+//"string"=>true
+
+let number=99
+let inboolean=Boolean(number)
+console.log(typeof(inboolean))
+console.log(inboolean)
+
+//any number in boolean=> true
