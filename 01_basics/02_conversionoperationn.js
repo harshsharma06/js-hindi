ad966dd48a7a6db64f5e13266192aefc316b5a3a
harsh shrama <161470818+harshsharma06@users.noreply.github.com>
2024-03-01 18:33:31 +0000

    datatypes in js

--- 01_basics/02_conversionoperationn.js
+++ 01_basics/02_conversionoperationn.js
@@ -1,12 +1,12 @@
 let score="69abc"
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
 let ValueinNumber= Number(score)
 
-console.log(typeof(ValueinNumber));
+// console.log(typeof(ValueinNumber));
 
-console.log(ValueinNumber)//Nan=>not a number
+// console.log(ValueinNumber)//Nan=>not a number
 
 //1=>true, 0=>false
 //""=>false
@@ -14,7 +14,38 @@ console.log(ValueinNumber)//Nan=>not a number
 
 let number=99
 let inboolean=Boolean(number)
-console.log(typeof(inboolean))
-console.log(inboolean)
+// console.log(typeof(inboolean))
+// console.log(inboolean)
 
 //any number in boolean=> true
+
+// ========================OPERATIONS========================
+
+console.log(2+2)
+console.log(2-2)
+console.log(2/2)
+console.log(2*2)
+console.log(2**3)
+console.log(4%2)
+
+console.log("harsh "+"Sharma")
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log("1"+2+3+"2")
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
